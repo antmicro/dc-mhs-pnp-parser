c4244705c5929b4ec980015ff497eeab4743318b
Sebastian Mroz <smroz@antmicro.com>
2025-06-02 23:53:00 +0200

    [#78314] Generate data model and use it to deserialize json

--- src/pnp_parser/main.py
+++ src/pnp_parser/main.py
@@ -2,6 +2,7 @@ import typer
 import json
 from pathlib import Path
 from pipeline_manager.specification_builder import SpecificationBuilder
+from .fru_model import FRU
 
 SPECIFICATION_VERSION = '20240723.13'
 ASSETS_DIRECTORY = Path("./assets")
@@ -14,12 +15,14 @@ specification_builder = SpecificationBuilder(
 
 app = typer.Typer()
 
+
+
 @app.command()
 def main(fru_json: str, output_spec: str):
     with open(fru_json) as f:
         hpm_data = json.load(f)
-    for node in hpm_data["HPM"]:
-        print(node)
+    fru = FRU.model_validate(hpm_data)
+    print(fru.HPM.BoardInfo.Manufacturer)
 
 if __name__ == "__main__":
     app()

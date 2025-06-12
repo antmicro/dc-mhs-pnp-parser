fea9e444647220dd27629aac8951b38f39cda560
Michal Kotkowski <mkotkowski@antmicro.com>
2025-06-12 12:29:04 +0200

    [#78904] Add script for updating nodes position in graph based on json file

--- src/graph_aligner/graph_aligner.py
+++ src/graph_aligner/graph_aligner.py
@@ -26,5 +26,31 @@ def read(graph_path: str, position_json_path: str) -> None:
         json.dump(position_data, f, sort_keys=True, indent=4)
 
 
+@app.command()
+def write(position_json_path: str, graph_path: str) -> None:
+    position_json = Path(position_json_path)
+    if not position_json.exists():
+        print(f"File not found. Aborting!\n{str(position_json)}")
+        return
+    with open(position_json) as f:
+        position_data = json.load(f)
+
+    graph_data = Path(graph_path)
+    if not graph_data.exists():
+        print(f"File not found. Aborting!\n{str(graph_data)}")
+        return
+    with open(graph_data) as f:
+        graph = json.load(f)
+
+    graph_nodes = graph["graphs"][0]["nodes"]
+    for node_name in position_data:
+        position = position_data[node_name]
+        graph_node = [node for node in graph_nodes if node["name"] == node_name][0]
+        graph_node.update({"position": position})
+
+    with open(graph_data, "w") as f:
+        json.dump(graph, f, sort_keys=True, indent=4)
+
+
 if __name__ == "__main__":
     app()
